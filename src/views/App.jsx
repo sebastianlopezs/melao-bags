f74359e388a7046b74124e74011260f7484425cc
bynx <sebastiancastrorajbe@gmail.com>
2023-12-03 21:50:21 -0400

    botton agregado

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -3,18 +3,20 @@ import "../css/app.css";
 export const App = () => {
   return (
     <>
-      <div className="mt-28"></div>
-      <img src="/landingpage1.jpg" className="" />
+      <div className="mt-32"></div>
+      <img src="/landingpage1.jpg" className="w-[70rem] m-auto" />
       <div className="flex justify-center w-full my-5">
         <button className="w-40 border border-black hover:border-red-600 text-black hover:text-red-600 transition duration-300 rounded-full p-3 text-xl">
           Explorar
         </button>
       </div>
-      <img src="/landingpage2.png" className="" />
-      <div className="flex justify-center w-full my-5">
-        <button className="w-40 border border-black hover:border-red-600 text-black hover:text-red-600 transition duration-300 rounded-full p-3 text-xl">
-          Personalizar
-        </button>
+      <div className="w-screen">
+        <div className="flex justify-center w-full my-5">
+          <button className=" w-40 border border-neutral-100 hover:border-white text-neutral-100 hover:text-white hover:bg-neutral-400 hover:bg-opacity-25 transition duration-300 rounded-full p-3 text-xl absolute z-50 mt-[48rem]">
+            Personalizar
+          </button>
+          <img src="/landingpage2.png" className="" />
+        </div>
       </div>
     </>
   );
